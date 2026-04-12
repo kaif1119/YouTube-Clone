f1808280430d457b1af43658b551307b705190b0
Kaif1119 <mohdkaifsiddiqui1196@gmail.com>
2026-04-13 04:41:42 +0530

    file path change

--- project/src/app/main.js
+++ project/src/app/main.js
@@ -1,10 +1,10 @@
-import { youtubeVideos } from "./data/videos.js";
-import { renderVideos } from "./components/renderVideos.js";
-import { initSearch } from "./features/search.js";
-import { initChips } from "./features/chips.js";
-import { initTheme } from "./features/themeToggle.js";
-import { initOfflineStatus } from "./features/offlineStatus.js";
-import { initMobileSearch } from "./features/mobileSearch.js";
+import { youtubeVideos } from "../data/videos.js";
+import { renderVideos } from "../components/renderVideos.js";
+import { initSearch } from "../features/search.js";
+import { initChips } from "../features/chips.js";
+import { initTheme } from "../features/themeToggle.js";
+import { initOfflineStatus } from "../features/offlineStatus.js";
+import { initMobileSearch } from "../features/mobileSearch.js";
 
 // DOM
 const container = document.getElementById("videos");
